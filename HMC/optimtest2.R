21f122a5ed92bc1dcd0097e72e7d5ab721397b07
Shagun Rawat <shagunrawat27@gmail.com>
2016-03-25 23:34:10 -0700

    Fixed the gradient of the posterior

--- HMC/optimtest2.R
+++ HMC/optimtest2.R
@@ -1,7 +1,5 @@
 rm(list = ls(all = TRUE))
 
-library(MASS)
-
 # load data
 load('fakedata_ou_noise.RData')
 fd = xtraj
@@ -9,18 +7,29 @@ fd = xtraj
 # load necessary functions
 source('dtq_with_grad.R')
 
-objgradfun <- function(c0)
+# define log prior
+logprior <- function(z, mu, mass_sd)
+{
+    return(dnorm(x = z, mean = mu, sd = mass_sd, log = TRUE))
+}
+
+objgradfun <- function(c0, z, mu, mass_sd)
 {
     probmat = cdt(c0, h = myh, k = myk, bigm = mybigm, littlet = 1, data = fd)
+
     mylik = probmat$lik
     mylik[mylik < 0] = 0
-    objective = -sum(log(mylik))
-    nc0 = length(c0)
-    gradient = numeric(length=nc0)
-    for (i in c(1:nc0))
-        gradient[i] = -sum(probmat$grad[[i]] / probmat$lik)
 
-    return(list("objective"=objective,"gradient"=gradient))
+    objective = - sum(log(mylik))
+
+    nc0 = length(c0)
+    gradient = numeric(length = nc0)
+
+    for (i in c(1:nc0))
+        gradient[i] = - sum(probmat$grad[[i]] / probmat$lik)
+
+    gradient = gradient - (z - mu)/(mass_sd)^2
+    return(list("objective" = objective, "gradient" = gradient))
 }
 
 # create grid, compute densities on that grid
@@ -32,39 +41,41 @@ mybigm = ceiling(pi/(myk^1.5))
 theta = c(1, 2, 1)
 numparam = length(theta)
 
-totsteps = 1000
-# OLD: Metropolis, NEW: HMC
+totsteps = 10
+artrack = {}
 
 for (i in c(1:(totsteps-1)))
 {
     # generate proposal for the momentum term, phi is numparam dimensional 
-    # mu is a vector of means and sigma is the covariance matrix
-    phi = mvrnorm(n = numparam, mu = 0, sd = 0.25)
-	
-#     # calculate likelihood and posterior density
-#     thetavec = truethetavec
-#     thetavec[1] = prop
-#     propden = Rdtq2d(thetavec,C1,C2,h=myh,numsteps=myns,k=myk,yM=xylimit)
-#     proppost = myposterior(likden=propden, dat=X, prior=myprior(prop)) 
-#     rho = exp(proppost-oldpost)
-#     maxcolsumerr = max(abs(colSums(propden)*myk^2 - 1))
+    mass_sd = 0.25
+    mu = 0
+    phi = rnorm(n = numparam, mean = mu, sd = mass_sd)
 
-#     # accept/reject step
-#     u = runif(n = 1)
-#     if (rho > u)
-#     {
-#         x[i+1] = prop
-#         oldden = propden
-#         oldpost = proppost
-#         print(paste("Accepted the proposal",prop))
-#         artrack[i] = 1
-#     }
-#     else
-#     {
-#         x[i+1] = x[i]
-#         print(paste("Rejected the proposal",prop))
-#         artrack[i] = 0
-#     }
-#     print(c(i,rho,maxcolsumerr,x[i+1]))
-#     flush.console()
+    oldlogprior = logprior(phi, mu, mass_sd)
+    oldgradpost = objgradfun(theta, phi, mu, mass_sd)
+
+    phi_half = phi + (oldgradpost$gradient) * (myh / 2)
+    theta_star = theta + phi_half * (myh / mass_sd)
+
+    propgradpost = objgradfun(theta_star, phi_half, mu, mass_sd)
+    phi_star = phi_half + (propgradpost$gradient) * (myh / 2)
+
+    proplogprior = logprior(phi_star, mu, mass_sd)
+
+	rho = (exp(propgradpost$objective) * exp(proplogprior)) / (exp(oldgradpost$objective) * exp(oldlogprior))
+
+    # accept/reject step
+    u = runif(n = 1)
+    if (rho > u)
+    {
+        theta = theta_star
+        oldgradpost = propgradpost
+        print(paste("Accepted the proposal",prop))
+        artrack[i] = 1
+    }
+    else
+    {
+        print(paste("Rejected the proposal",prop))
+        artrack[i] = 0
+    }
 }
