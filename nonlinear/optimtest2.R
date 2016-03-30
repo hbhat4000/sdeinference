a4a2240f208d6fc9bf5bc163221c38f24d47b432
Shagun Rawat <shagunrawat27@gmail.com>
2016-03-30 13:47:17 -0700

    Generating simulations

--- nonlinear/optimtest2.R
+++ nonlinear/optimtest2.R
@@ -1,7 +1,7 @@
 rm(list = ls(all = TRUE))
 
 # load data
-load('fakedata_ou_noise.RData')
+load('fakedata5.RData')
 fd = xtraj
 
 # load necessary functions
@@ -16,20 +16,20 @@ objgradfun <- function(c0)
     nc0 = length(c0)
     gradient = numeric(length=nc0)
     for (i in c(1:nc0))
-        gradient[i] = -sum(probmat$grad[[i]] / probmat$lik)
+        gradient[i] = - sum(probmat$grad[[i]] / probmat$lik)
 
     return(list("objective"=objective,"gradient"=gradient))
 }
 
 # create grid, compute densities on that grid
-myh = 0.1
-myk = myh^0.75
+myh = 0.01
+myk = myh
 mybigm = ceiling(pi/(myk^1.5))
 
 # initial condition fakedata = c(1,4,0.5)
-theta = c(1, 2, 1)
+theta = c(4, 4, 0.5)
 
 library('nloptr')
 
-res <- nloptr(x0 = theta, eval_f = objgradfun, lb = c(0.1, 0, 0.1), ub = c(10, 10, 1.0), opts = list("algorithm"="NLOPT_LD_LBFGS", "print_level"=3, "check_derivatives" = TRUE, "xtol_abs"=1e-3))
+res <- nloptr(x0 = theta, eval_f = objgradfun, lb = c(0, 0, 0.01), ub = c(10, 10, 2), opts = list("algorithm"="NLOPT_LD_LBFGS", "print_level"=3, "check_derivatives" = TRUE, "xtol_abs"=1e-4))
 
