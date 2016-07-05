d282da7a173269efbc67cfce3091ce04d0d84c24
Shagun Rawat <shagunrawat27@gmail.com>
2016-07-05 14:17:19 -0700

    Randomized runner trajectory

--- simulation2d/makefakedata.R
+++ simulation2d/makefakedata.R
@@ -2,22 +2,28 @@ rm(list = ls(all = TRUE))
 
 # creating the runner's trajectory
 t = 1
-T = 10
+T = 100
 tvec = seq(from = 0, to = T, by = t)
 xrun = seq(from = 1, to = 94, by = 0.001)
-yrun = 5*log(xrun) 
+# yrun = 5*log(xrun) 
+yrun = rnorm(n = length(xrun), mean = 0, sd = 2) + xrun*xrun/500 + xrun/20 + 5
 xrun = xrun[seq(from = 1, to = length(xrun), length.out = T+t)]
 yrun = yrun[seq(from = 1, to = length(yrun), length.out = T+t)]
+
+# Basketbal court's dimension
 xaxis = c(0,94)
 yaxis = c(0,50)
-plot(xrun, yrun, xaxis, yaxis, type = "o", col = "red")
-lines(xrun, yrun, type = "l", col = "blue")
+# plot(xrun, yrun, xaxis, yaxis, type = "o", col = "red")
+plot(xrun, yrun, xaxis, yaxis, type = "l", col = "red")
+# lines(xrun, yrun, type = "l", col = "blue")
 runner = list(tvec, xrun, yrun)
 
 # setting the parameters for the pursuit model
 speed <- function(t)
 {
-  sval = 1 + 2*t
+  # sval = 1 + 2*t
+  # sval = -1 + 2*t + rnorm(n = length(t))
+  sval = 1.2 + rnorm(n = length(t))
   return(sval)
 }
 
