150ba889242b186336406a38b8f90532c3fe6407
Harish Bhat <hbhat@ucmerced.edu>
2016-11-14 13:53:32 -0800

    Adding simple code to evaluate log likelihood using pomp's pfilter

---
+++ pursuit2d/pomp/pompv2.R
@@ -0,0 +1,42 @@
+rm(list=ls(all=TRUE))
+library('pomp')
+library('magrittr')
+library('dplyr')
+
+# for reproducibility
+set.seed(1)
+
+load('newfakedata_fullres.RData')
+mydata = X[seq(from=1,to=2001,by=40),]
+mymod.dat = data.frame(t=mydata[,3],Y1=as.numeric(mydata[,1]),Y2=as.numeric(mydata[,2]))
+
+ptm = proc.time()
+
+step.fun <- Csnippet("
+ double dW1 = rnorm(0,sqrt(dt));
+ double dW2 = rnorm(0,sqrt(dt));
+ X1 += -theta1*theta1*X2*dt + (0.16/(2*M_PI))*theta1*theta1*dW1;
+ X2 += theta2*theta2*X1*dt + (0.16/(2*M_PI))*theta2*theta2*dW2;
+")
+
+mymod <- pomp(data=mymod.dat,time="t",t0=0,
+              rprocess=euler.sim(step.fun=step.fun,delta.t=0.05),
+              statenames=c("X1","X2"),paramnames=c("theta1","theta2"))
+
+rmeas <- Csnippet("
+ Y1 = X1 + rnorm(0,1e-2);
+ Y2 = X2 + rnorm(0,1e-2);
+")
+
+mymod <- pomp(mymod,rmeasure=rmeas,statenames=c("X1","X2"))
+
+dmeas <- Csnippet("
+  lik = dnorm(Y1,X1,1e-2,1) + dnorm(Y2,X2,1e-2,1);
+")
+
+mymod <- pomp(mymod,dmeasure=dmeas,statenames=c("X1","X2"))
+
+pf <- pfilter(mymod,Np=1000,params=c(theta1=2,theta2=2,X1.0=mymod.dat[1,]$Y1,X2.0=mymod.dat[1,]$Y2))
+print(logLik(pf))
+
+
