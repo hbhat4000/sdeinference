97ec535f7d17324fc0219222b1c9a024cee3a478
Harish S. Bhat <hbhat@ucmerced.edu>
2016-11-22 11:43:06 -0800

    Adding van der Pol simulated data folder

---
+++ oldruns2d/revisiondata/vanderpol/truethetavec_anharmonic.R
@@ -0,0 +1,10 @@
+truethetavec = numeric(length = 5)
+# truethetavec[1] = 2*pi
+# truethetavec[2] = 2*pi
+# truethetavec[3] = 2*pi
+truethetavec[1:2] = 1
+truethetavec[3] = 4
+truethetavec[4] = .5
+truethetavec[5] = .5
+
+# 2*pi = 6.283185, .4/sqrt(2*pi) = 0.1595769, (.4/sqrt(2*pi))^2 = 0.02546479
