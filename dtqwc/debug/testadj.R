61e675315c7027f9681682cd7f5e0c66e2e3f47b
Harish S. Bhat <hbhat@ucmerced.edu>
2017-02-17 16:34:33 -0800

    Added data to the cpp file, added some helpful bits to the README file

--- dtqwc/debug/testadj.R
+++ dtqwc/debug/testadj.R
@@ -2,12 +2,12 @@ rm(list = ls(all = TRUE))
 
 # load data
 load('fakedata.RData')
-fd = xtraj[1:4,1:4]
+fd = xtraj[1:4,]
 
 # load necessary functions
 source('adjoint.R')
 
-myh = 0.1
+myh = 0.01
 myk = myh^0.75
 mybigm = ceiling(pi/(myk^1.5))
 
